20f13689c07a9e02e4ca87c6ebce37b1d77ecf14
Kacper Gruca <kacpergruca@gmail.com>
2025-06-04 00:11:40 +0200

    Add user profiles

---
+++ src/comparisons/user_profiles.py
@@ -0,0 +1,26 @@
+USER_PROFILES = {
+    "EcoFriendly": {
+        "co2_emission_kg": -0.5,
+        "cost_pln": -0.2,
+        "normalized_comfort": -0.2,
+        "normalized_failure_rate": -0.1,
+    },
+    "Saver": {
+        "co2_emission_kg": -0.1,
+        "cost_pln": -0.6,
+        "normalized_comfort": -0.1,
+        "normalized_failure_rate": -0.2,
+    },
+    "ComfortSeeker": {
+        "co2_emission_kg": -0.1,
+        "cost_pln": -0.2,
+        "normalized_comfort": -0.5,
+        "normalized_failure_rate": -0.2,
+    },
+    "Balanced": {
+        "co2_emission_kg": -0.25,
+        "cost_pln": -0.25,
+        "normalized_comfort": -0.25,
+        "normalized_failure_rate": -0.25,
+    }
+}
